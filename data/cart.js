ece3caccbe2ce558a905e9f54105756cc5405437
demba <demba7315@gmail.com>
2024-08-01 12:28:59 +0000

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -5,3 +5,31 @@ export const cart = [{
     productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
 }];
+
+export function addToCart(productId) {
+
+    let matchingItem;
+
+
+       cart.forEach( (cartItem) => {
+        if (productId === cartItem.productId) {
+
+            matchingItem = cartItem;
+            
+        }
+       });
+
+       if (matchingItem) {
+        matchingItem.quantity +=1;
+
+       }else{
+
+        cart.push({
+            productId:productId,
+            quantity: 1
+           });
+
+       }
+
+    
+}
